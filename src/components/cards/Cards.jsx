c6425d4ac4f8d1d8dc00c8fe1294a9a3d834a041
Manu <manuel_8ale@hotmail.com>
2025-03-03 18:57:01 -0400

    fix cards data error

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,41 +1,41 @@
 import Card from "./Card";
 
+const formatNumber = (nString) => {
+  return nString ? parseFloat(nString).toPrecision(3) : "--";
+};
+
 export default function Cards({ data = {} }) {
   return (
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mt-4">
       <Card
         title="temperatura"
-        value={data?.temperatura.toPrecision(3) || "--"}
+        value={formatNumber(data?.temperatura)}
         variable="temperatura"
       />
       <Card
         title="humedad"
-        value={data?.humedad.toPrecision(3) || "--"}
+        value={formatNumber(data?.humedad)}
         variable="humedad"
       />
       <Card
         title="Presión"
-        value={data?.presion.toPrecision(3) || "--"}
+        value={formatNumber(data?.presion)}
         variable="presion"
       />
-      <Card
-        title="solar"
-        value={data?.solar.toPrecision(3) || "--"}
-        variable="solar"
-      />
+      <Card title="solar" value={formatNumber(data?.solar)} variable="solar" />
       <Card
         title="pluviosidad"
-        value={data?.pluviosidad.toPrecision(3) || "--"}
+        value={formatNumber(data?.pluviosidad)}
         variable="pluviosidad"
       />
       <Card
         title="Velocidad del viento"
-        value={data?.velocidad.toPrecision(3) || "--"}
+        value={formatNumber(data?.velocidad)}
         variable="vientoVelocidad"
       />
       <Card
         title="Dirección del viento"
-        value={data?.direccion.toPrecision(3) || "--"}
+        value={formatNumber(data?.direccion)}
         variable="vientoDireccion"
       />
     </div>
