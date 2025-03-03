1454de5d1dd928744a8433685811b5b3b40b3f0d
Manu <manuel_8ale@hotmail.com>
2025-03-02 22:37:14 -0400

    fix display cards data structure

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,41 +1,41 @@
 import Card from "./Card";
 
-export default function Cards({ data }) {
+export default function Cards({ data = {} }) {
   return (
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mt-4">
       <Card
         title="temperatura"
-        value={data?.temperatura?.valor.toPrecision(3) || "--"}
+        value={data?.temperatura.toPrecision(3) || "--"}
         variable="temperatura"
       />
       <Card
         title="humedad"
-        value={data?.humedad?.valor.toPrecision(3) || "--"}
+        value={data?.humedad.toPrecision(3) || "--"}
         variable="humedad"
       />
       <Card
         title="Presión"
-        value={data?.presion?.valor.toPrecision(3) || "--"}
+        value={data?.presion.toPrecision(3) || "--"}
         variable="presion"
       />
       <Card
         title="solar"
-        value={data?.solar?.valor.toPrecision(3) || "--"}
+        value={data?.solar.toPrecision(3) || "--"}
         variable="solar"
       />
       <Card
         title="pluviosidad"
-        value={data?.pluviosidad?.valor.toPrecision(3) || "--"}
+        value={data?.pluviosidad.toPrecision(3) || "--"}
         variable="pluviosidad"
       />
       <Card
         title="Velocidad del viento"
-        value={data?.velocidad?.valor.toPrecision(3) || "--"}
+        value={data?.velocidad.toPrecision(3) || "--"}
         variable="vientoVelocidad"
       />
       <Card
         title="Dirección del viento"
-        value={data?.direccion?.valor.toPrecision(3) || "--"}
+        value={data?.direccion.toPrecision(3) || "--"}
         variable="vientoDireccion"
       />
     </div>
