3745e973ddc8374216ab2c28be3fff32db4ba8cc
Manu <manuel_8ale@hotmail.com>
2025-03-11 13:18:23 -0400

    resize connection indicator

--- src/app.jsx
+++ src/app.jsx
@@ -108,19 +108,19 @@ export function App() {
             </div>
             <div>
               {readyState === ReadyState.CLOSED && (
-                <span class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">
+                <span class="inline-flex items-center bg-red-100 text-red-800 text-sm font-medium me-2 px-3 py-1 rounded-full dark:bg-red-900 dark:text-red-300">
                   <span class="w-2 h-2 me-1 bg-red-500 rounded-full"></span>
                   Desconectado
                 </span>
               )}
               {readyState === ReadyState.CONNECTING && (
-                <span class="inline-flex items-center bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 animate-pulse">
+                <span class="inline-flex items-center bg-blue-100 text-blue-800 text-sm font-medium me-2 px-3 py-1 rounded-full dark:bg-blue-900 dark:text-blue-300 animate-pulse">
                   <span class="w-2 h-2 me-1 bg-blue-500 rounded-full"></span>
                   Conectando...
                 </span>
               )}
               {readyState === ReadyState.OPEN && (
-                <span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+                <span class="inline-flex items-center bg-green-100 text-green-800 text-sm font-medium px-3 py-1 rounded-full dark:bg-green-900 dark:text-green-300">
                   <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
                   Conectado
                 </span>
